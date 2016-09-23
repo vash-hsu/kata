8b4d4a3ec0f88e19286ec571975955934fa06f07
vash-hsu <vash.hsu@gmail.com>
2016-09-23 11:27:50 +0800

    support GET/POST/PUT/DELETE

--- 102_web_api/main.py
+++ 102_web_api/main.py
@@ -8,19 +8,20 @@ from flask import render_template
 
 import json
 import demjson
+import hashlib
 
 app = Flask(__name__)
 
-OP = {"GET" : "returns list of all objects",
-      "PUT" : "add item",
-      "POST" : "updates item",
-      "DELETE" : "deletes item"}
+OP = {"GET": "returns list of all objects",
+      "PUT": "updates item",
+      "POST": "adds item",
+      "DELETE": "deletes item"}
 
 ACCEPT_OP = ['GET', 'PUT', 'POST', 'DELETE']
 
-STORAGE = {"string":["hello", "world", ],
-           "int":[1, 2, 3],
-           "float":[1.1, 2.2, 3.3]}
+STORAGE = {
+    "123456789": "value for golden key",
+    "test_data": "value for test_data"}
 
 
 @app.route("/")
@@ -125,45 +126,90 @@ def api_help(operator):
     return html
 
 
+def _api_split_version_resource(string_path):
+    """
+    :param string_path:   v1/resource
+    :return:  ['v1', 'resource']
+    """
+    terms = string_path.split('/')
+    if len(terms) >= 2:
+        return terms
+    else:
+        return [string_path, ]
+
+
 # ===
-# /api/v1
+# /api/v1/resource
 # ===
-@app.route("/api/<version>", methods=ACCEPT_OP)
-def api(version):
-    if version != "v1":
-        return "version not supported yet"
+@app.route("/api/<path:resource_path>", methods=ACCEPT_OP)
+def api(resource_path):
+    terms = _api_split_version_resource(resource_path)
+    if len(terms) < 2:
+        return "undefined path /api/%s" % resource_path, 404
+    api_version = terms[0]
+    res_type = terms[1]
+    resource_id = None
+    if len(terms) == 3:
+        resource_id = terms[2]
+    if api_version != "v1":
+        return "API version not supported yet"
+    if res_type != "resource":
+        return "resource type not defined"
+    # depends on request method
     _method = request.method
     if _method not in ACCEPT_OP:
-        return "method not supported yet"
-    response_body = ""
-    if _method == "GET":
-        response_body = _api_get()
+        response_body = demjson.encode({"status": "-1",
+                                        'reason': '%s not supported' % _method})
+    elif _method == "GET":
+        response_body = _api_get(resource_id)
     else:
         response_body = _handle_api_and_json(_method,
                                              request.headers,
-                                             request.data)
+                                             request.data,
+                                             resource_id)
     # return JSON
     resp = Response(response_body)
     resp.headers['Content-type'] = "application/json"
     return resp
 
 
-def _handle_api_and_json(method, headers, data):
+def _handle_api_and_json(method, headers, data, resource_id=None):
     """
     :param method:  PUT/POST/DELETE
     :param headers:  Content-Type
     :param data:  JSON
+    :param resource_id:  string
     :return: JSON
     """
+    error_msg = []
+    json_in_dict = dict()
     if not _is_content_type_json(headers):
-        return demjson.encode({'status': 'ERROR'})
-    if method == "PUT":
-        return _api_put(data)
-    elif method == "POST":
-        return demjson.encode({'status': 'NOTYET', 'OP': method})
-    elif method == "DELETE":
-        return demjson.encode({'status': 'NOTYET', 'OP': method})
-    return demjson.encode({'status': 'ERROR'})
+        error_msg.append('should provide content-type as application/json')
+    else:
+        try:
+            json_in_dict = demjson.decode(data)
+        except BaseException:
+            error_msg.append('fail to decode incoming json')
+    if len(error_msg) == 0:
+        if method == "PUT":
+            if _is_valid_id(resource_id):  # update legacy
+                return _api_put(json_in_dict, resource_id)
+            else:
+                error_msg.append("PUT needs existent resource_id")
+        elif method == "POST":  # insert new
+            if resource_id is None or len(resource_id) == 0:
+                return _api_post(json_in_dict)
+            else:
+                error_msg.append("POST needs not user-specified resource_id")
+        elif method == "DELETE":
+            if _is_valid_id(resource_id):  # delete legacy
+                return _api_delete(json_in_dict, resource_id)
+            else:
+                error_msg.append("DELETE needs existent resource_id")
+        else:
+            error_msg.append('not support method with %s' % repr(resource_id))
+    return demjson.encode({'status': str(0-len(error_msg)),
+                           'reason': error_msg})
 
 
 def _is_content_type_json(header_dict):
@@ -180,29 +226,90 @@ def _is_content_type_json(header_dict):
 # ===
 # api implementation
 # ===
-def _api_get():
-    # return "</br>".join(STORAGE.keys())
-    json_meta = demjson.encode(STORAGE)
+
+# Get: return list of all objects' names
+def _api_get(resource_id=None):
+    """
+    :param resource_id: string
+    :return:
+        'status', number of elements in STORAGE
+        'storage', elements in STORAGE
+    """
+    dict_for_return = dict()
+    if not resource_id: # return all elements' name
+        dict_for_return['status'] = len(STORAGE)
+        dict_for_return['listing'] = STORAGE.keys()
+    elif resource_id in STORAGE:
+        dict_for_return['status'] = '1'
+        dict_for_return['value'] = STORAGE[resource_id]
+    else: # unrecognized id
+        dict_for_return['status'] = '0'
+    json_meta = demjson.encode(dict_for_return)
     return json_meta
 
 
-def _api_put(json_meta=None):
-    input_json = demjson.decode(json_meta)
-    return demjson.encode({'status': 'ECHO',
-                           'OP': "PUT",
-                           'INPUT': input_json})
+def _is_valid_id(string_id):
+    if not string_id:
+        return False
+    if string_id in STORAGE:
+        return True
+    return False
 
 
-def _api_post(json_meta=None):
-    return OP['POST']
+def _update_to_internal_storage(id, data):
+    STORAGE[id] = data
 
 
-def _api_delete(json_meta=None):
-    return OP['DELETE']
+def _remove_from_internal_storage(id):
+    del STORAGE[id]
 
 
+def _get_sha1_in_string(data):
+    sha1 = hashlib.sha1()
+    sha1.update(repr(data))
+    return sha1.hexdigest()
 
+
+# PUT: updates item
+# update name:value, where name should be in STORAGE already
+def _api_put(json_meta, resource_id=None):
+    """
+    :param json_meta: {"item name": item value}
+    :param resource_id: string
+    :return: json
+    """
+    _update_to_internal_storage(id=resource_id, data=json_meta)
+    status = '1'
+    return_data = demjson.encode({'status': status, 'id': resource_id})
+    return return_data
+
+
+# POST: adds item
+# update name:value, where name was created in runtime
+def _api_post(json_meta):
+    """
+    :param json_meta:
+    :return: json
+    """
+    resource_id = _get_sha1_in_string(json_meta)
+    _update_to_internal_storage(id=resource_id, data=json_meta)
+    status = '1'
+    return_data = demjson.encode({'status': status, 'id': resource_id})
+    return return_data
+
+
+# DELETE: deletes item
+def _api_delete(json_meta, resource_id):
+    """
+    :param json_meta:
+    :param resource_id:
+    :return:
+    """
+    _remove_from_internal_storage(resource_id)
+    status = '1'
+    return_data = demjson.encode({'status': status, 'id': resource_id})
+    return return_data
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- 102_web_api/test_main.py
+++ 102_web_api/test_main.py
@@ -9,7 +9,7 @@ import urllib
 import json
 import demjson
 
-class TestFlashRequest(unittest.TestCase):
+class TestFlaskRequest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -29,17 +29,158 @@ class TestFlashRequest(unittest.TestCase):
         self.assertEqual(header['status'], '200')
         self.assertIn("Welcome", body)
 
-    def test_http_method_put(self):
-        "=== PUT send JSON, and get JSON back"
-        target_url = self.host_url + "/api/v1"
-        prepared_body = """{"message": "Hello Data"}"""
+    def test_api_split_version_resource(self):
+        "input v1/resource, return [v1, resource]"
+        target_path = "v1/resource"
+        results = main._api_split_version_resource(target_path)
+        self.assertEqual(results, ['v1', 'resource'])
+        target_path = "v1/resource/123456789"
+        results = main._api_split_version_resource(target_path)
+        self.assertEqual(results, ['v1', 'resource', '123456789'])
+
+    def test_api_v1_api_get_all_elements_list(self):
+        "=== GET and then get JSON back"
+        target_url = self.host_url + "/api/v1/resource"
+        header, body = self.handle.request(target_url, 'GET')
+        self.assertEqual(header['status'], '200')
+        self.assertEqual(header['content-type'], "application/json")
+        json_dict = demjson.decode(body)
+        self.assertGreater(int(json_dict["status"]), 0)
+
+    def test_api_v1_api_get_element_by_id(self):
+        "=== GET and then get JSON back"
+        target_url = self.host_url + "/api/v1/resource/123456789"
+        header, body = self.handle.request(target_url, 'GET')
+        self.assertEqual(header['status'], '200')
+        self.assertEqual(header['content-type'], "application/json")
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict["status"], '1')
+        self.assertIn("golden key", json_dict["value"])
+
+    def test_api_v1_api_get_element_by_not_found_id(self):
+        "=== GET  with undefined resource_id, and then get JSON back with status 0"
+        target_url = self.host_url + "/api/v1/resource/notdefined"
+        header, body = self.handle.request(target_url, 'GET')
+        self.assertEqual(header['status'], '200')
+        self.assertEqual(header['content-type'], "application/json")
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict["status"], '0')
+
+    def test_api_v1_put_by_existent_id(self):
+        "=== PUT /api/v1/resource/golden_data and get status=1 in jason back"
+        target_url = self.host_url + "/api/v1/resource/test_data"
+        prepared_body = demjson.encode({'new_element': 'new data'})
         prepared_header = {'Content-Type': 'application/json'}
         header, body = self.handle.request(target_url, "PUT",
                                            body=prepared_body,
                                            headers=prepared_header)
         self.assertEqual(header['status'], '200')
-        self.assertEqual(body,
-                         """{"INPUT":{"message":"Hello Data"},"OP":"PUT","status":"ECHO"}""")
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict['status'], '1')
+        self.assertEqual(json_dict['id'], 'test_data')
+
+    def test_api_v1_put_disallow_nonexistent_id(self):
+        "=== PUT /api/v1/resource/undefined and get status=-1 " \
+        "to indicate not allow to update non-existent resouroce-id"
+        target_url = self.host_url + "/api/v1/resource/undefined"
+        prepared_body = demjson.encode({'new_element': 'new data'})
+        prepared_header = {'Content-Type': 'application/json'}
+        header, body = self.handle.request(target_url, "PUT",
+                                           body=prepared_body,
+                                           headers=prepared_header)
+        self.assertEqual(header['status'], '200')
+        json_dict = demjson.decode(body)
+        self.assertLess(json_dict['status'], '0')
+        # print "DM: json_dict =", repr(json_dict)
+
+    def test_api_v1_post_by_creating_new_id_without_slash(self):
+        "=== POST /api/v1/resource and get status=1 after inserting new resource_id"
+        target_url = self.host_url + "/api/v1/resource"
+        prepared_body = demjson.encode({'ending_without_slash': 'dummy data'})
+        prepared_header = {'Content-Type': 'application/json'}
+        header, body = self.handle.request(target_url, "POST",
+                                           body=prepared_body,
+                                           headers=prepared_header)
+        self.assertEqual(header['status'], '200')
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict['status'], '1', 'ending without slash')
+        self.assertEqual(len(json_dict['id']), 40) # in form of sha1 string
+
+    def test_api_v1_post_by_creating_new_id_with_slash(self):
+        "=== POST /api/v1/resource/ and get status=1 after inserting new resource_id"
+        target_url = self.host_url + "/api/v1/resource/"
+        prepared_body = demjson.encode({'ending_with_slash': 'dummy data'})
+        prepared_header = {'Content-Type': 'application/json'}
+        header, body = self.handle.request(target_url, "POST",
+                                           body=prepared_body,
+                                           headers=prepared_header)
+        self.assertEqual(header['status'], '200')
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict['status'], '1', 'ending with slash')
+        self.assertEqual(len(json_dict['id']), 40) # in form of sha1 string
+
+    def test_api_v1_post_then_delete(self):
+        "=== POST /api/v1/resource/ and get resource_id back, then DELETE it"
+        target_url = self.host_url + "/api/v1/resource"
+        prepared_body = demjson.encode({'ha ha': 'create and then delete'})
+        prepared_header = {'Content-Type': 'application/json'}
+        header, body = self.handle.request(target_url, "POST",
+                                           body=prepared_body,
+                                           headers=prepared_header)
+        self.assertEqual(header['status'], '200')
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict['status'], '1')
+        working_resource_id = json_dict['id']
+        # then delete it
+        target_url = self.host_url + "/api/v1/resource" + \
+                     "/" + working_resource_id
+        prepared_body = demjson.encode({})
+        prepared_header = {'Content-Type': 'application/json'}
+        header, body = self.handle.request(target_url, "DELETE",
+                                           body=prepared_body,
+                                           headers=prepared_header)
+        self.assertEqual(header['status'], '200')
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict['status'], '1')
+        self.assertEqual(json_dict['id'],
+                         working_resource_id,
+                         "kill the wrong guy?")
+
+    def test_api_v1_delete_disallow_nonexistent_id(self):
+        "=== DELETE /api/v1/resource/undefined and get status=-1 " \
+        "to indicate not allow to delete non-existent resouroce-id"
+        target_url = self.host_url + "/api/v1/resource/undefined"
+        prepared_body = demjson.encode({})
+        prepared_header = {'Content-Type': 'application/json'}
+        header, body = self.handle.request(target_url, "DELETE",
+                                           body=prepared_body,
+                                           headers=prepared_header)
+        self.assertEqual(header['status'], '200')
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict['status'], '-1')
+        print "DM:", repr(json_dict)
+
+    def test_api_v1_delete_disallow_action_without_id(self):
+        "=== DELETE /api/v1/resource and get status=-1 "
+        target_url = self.host_url + "/api/v1/resource"
+        prepared_body = demjson.encode({})
+        prepared_header = {'Content-Type': 'application/json'}
+        header, body = self.handle.request(target_url, "DELETE",
+                                           body=prepared_body,
+                                           headers=prepared_header)
+        self.assertEqual(header['status'], '200')
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict['status'], '-1')
+        # with slash
+        target_url = self.host_url + "/api/v1/resource/"
+        prepared_body = demjson.encode({})
+        prepared_header = {'Content-Type': 'application/json'}
+        header, body = self.handle.request(target_url, "DELETE",
+                                           body=prepared_body,
+                                           headers=prepared_header)
+        self.assertEqual(header['status'], '200')
+        json_dict = demjson.decode(body)
+        self.assertEqual(json_dict['status'], '-1')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
